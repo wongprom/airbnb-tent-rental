9c06acfa0117a32852d568a49e16684ed242139d
wongprom <>
2021-08-17 10:03:51 +0200

    Cleaning starter template. Add color scheme.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,23 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      otr: {
+        primaryPurple: '#8A07BD',
+        primaryBlue: '#080AEF',
+        primaryOrange: '#F56407',
+        primaryWhite: '#F3F3F0',
+        primaryBlack: '#0C0C0A',
+        secondaryBeige: '#d0b280',
+        secondaryLightOrange: '#a84600',
+        secondaryDarkOrange: '#973529',
+        secondaryDarkBlue: '#2a334f',
+        secondaryGray: '#8c8c8c',
+      },
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+};
