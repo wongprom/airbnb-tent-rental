6253faac3359b932f3d20a6fdd649abe26a622b4
wongprom <>
2021-08-18 08:48:51 +0200

    Small changes

--- pages/index.js
+++ pages/index.js
@@ -8,29 +8,31 @@ import Footer from '../components/Footer';
 
 export default function Home({ exploreData, cardsData }) {
   return (
-    <div className="">
+    <div>
       <Head>
-        <title>Outdoor Tent Rental</title>
+        <title>airbnb clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
       <Banner />
 
-      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+      <main className="mx-auto px-8 sm:px-16 max-w-7xl">
         <section className="pt-6">
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            {exploreData?.map((item) => (
+            {exploreData.map(({ img, distance, location }) => (
               <SmallCard
-                key={item.location}
-                img={item.img}
-                distance={item.distance}
-                location={item.location}
+                key={img}
+                img={img}
+                distance={distance}
+                location={location}
               />
             ))}
           </div>
         </section>
+
         <section>
           <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
 
@@ -40,6 +42,7 @@ export default function Home({ exploreData, cardsData }) {
             ))}
           </div>
         </section>
+
         <LargeCard
           img="https://links.papareact.com/4cj"
           title="The Greatest Outdoors"
