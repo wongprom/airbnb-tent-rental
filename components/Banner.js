17c0eef4ed89087d4e18bf56a13c391aff97e4bd
wongprom <>
2021-09-03 14:01:49 +0200

    Change text in Banner component.

--- components/Banner.js
+++ components/Banner.js
@@ -10,9 +10,9 @@ function Banner() {
         objectPosition="bottom"
       />
       <div className="absolute top-1/2 w-full text-center">
-        <p className="text-sm sm:text-lg">Not sure what gear to get? Perfect</p>
+        <p className="text-sm sm:text-lg">Not sure where to go? Perfect</p>
         <button className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-lg active:scale-90 transition duration-150">
-          View gear
+          I'm flexible
         </button>
       </div>
     </div>
